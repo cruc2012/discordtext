896bc35410ff5e119a755645350f62100faa2376
curtis <cjgann28122012@gmail.com>
2025-06-15 04:28:40 -0500

    Add files via upload

---
+++ publishers/mod-store/discordtext/info.js
@@ -0,0 +1,15 @@
+const appInfo = {
+  name: "Discord Text",
+  version: "0.1",
+  publisher: "mod-store",
+  size: "50 MB",
+  description: "Test",
+  features: "Test",
+  icon: "/publishers/mod-store/discordtext/logo.png",
+  screenshots: [
+    "/publishers/mod-store/discordtext/img1.png",
+    "/publishers/mod-store/discordtext/img2.png",
+    "/publishers/mod-store/discordtext/img3.png",
+  ],
+  downloadLink: "/publishers/mod-store/discordtext/download.apk"
+};
